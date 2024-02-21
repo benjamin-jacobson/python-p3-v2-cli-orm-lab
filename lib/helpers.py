9e5884019d96b64745d983ebdf966c85c6803e78
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-02-20 22:46:14 -0500

    wip

--- lib/helpers.py
+++ lib/helpers.py
@@ -68,27 +68,81 @@ def delete_department():
 # You'll implement the employee functions in the lab
 
 def list_employees():
-    pass
+    '''The function should get all employees stored in the database, then print each employee on a new line.'''
+    employees = Employee.get_all()
+    for e in employees:
+        print(e)
 
 
 def find_employee_by_name():
-    pass
-
+    '''should prompt for a name and then find the Employee instance with that 
+    name and print their information, or print an error message if the employee does not exist.'''
+    name_ = input("Enter the employee's name: ")
+    e = Employee.find_by_name(name_)
+    print(e) if e else print(f'Employee {name_} not found')
 
 def find_employee_by_id():
-    pass
-
+    '''should prompt for an id and then find the Employee instance with that id and print their 
+    information, or print an error message if the employee does not exist.'''
+    _id = input("Hey, enter the id of the employee: ")
+    e = Employee.find_by_id(_id)
+    print(e) if e else print(f'Employee of id {_id} not found.')
 
 def create_employee():
-    pass
-
+    '''Prompt for and read in a name, job title, and department id.
+        Create and persist a new Employee class instance, surrounding the code in a try/except 
+        block in case an exception is thrown by the name, job_title, or department_id property setter methods.
+        Print a message indicating that the Employee object was successfully created, or print an 
+        error message if an exception is thrown.
+    '''
+    
+    
+    name = input("Enter the employee's name: ")
+    job_title = input("Enter the employee's job title: ")
+    _id = int(input("Enter the employee's department id: "))
+    try:
+        e = Employee.create(name, job_title, _id)
+        print(f'Success: {e}')
+    except Exception as exc:
+        print("Error creating employee: ", exc)
 
 def update_employee():
-    pass
+    '''
+    Prompt for and read in the employee id.
+    Print an error message if the employee is not in the database. If the employee is in the database, attempt to do the following steps within a try-except block to catch any exceptions, printing an error message if an exception is thrown.
+    Prompt for a new name to update the name attribute (property setter may throw an exception).
+    Prompt for a new job title to update the job_title attribute (property setter may throw an exception).
+    Prompt for the employee's new department id to update the department_id attribute (property setter may throw an exception).
+    Update the employee in the database.
+    Print a success message after a successful update, or print an appropriate error message if an exception is thrown.
+    '''
+    id_ = int(input("Enter the employee's id: "))
+    if employee := Employee.find_by_id(id_):
+        try:
+            name = input("Enter the employee's new name: ")
+            employee.name = name
+            job = input("Enter the employee's new job title: ")
+            employee.job_title = job
+
+            employee.update()
+            print(f'Success: {employee}')
+        except Exception as exc:
+            print("Error updating employee: ", exc)
+    else:
+        print(f'employee {id_} not found')
 
 
 def delete_employee():
-    pass
+    '''
+    prompt for the employee id and delete the employee from the database 
+    if it exists and print a confirmation message, or print an error message if the employee is not in the database.
+    '''
+    id_ = int(input("Enter the department's id: "))
+    if emp := Employee.find_by_id(id_):
+        emp.delete()
+        print(f'emp {id_} deleted')
+    else:
+        print(f'emp {id_} not found')
 
 
 def list_department_employees():
